dd7699b36c9e05c664dc7805f753ab6f1afcc0d6
Amos Endersen <amenders@iupui.edu>
2017-11-13 21:25:43 -0500

    about to try changing blueprint-socket.io package manifest

--- app/routers/MainRouter.js
+++ app/routers/MainRouter.js
@@ -2,5 +2,9 @@ module.exports = exports = {
   '/helloworld' : {
     get  : { view   : 'helloworld.pug' },
     post : { action : 'HelloWorldController@echoName'},
+  },
+
+  '/messages' : {
+    get : {action : 'ChatController'}
   }
 };
