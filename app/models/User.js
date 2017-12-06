649854f47f84c3437508ed709bd6659562a09dca
Franz <defranz@indiana.edu>
2017-12-06 14:54:19 -0500

    updated and correct models router and controller

--- app/models/User.js
+++ app/models/User.js
@@ -6,14 +6,14 @@ var mongodb = require ('@onehilltech/blueprint-mongodb')
 // use mongodb.Types to access mongoose.Types
 
 var schema = new mongodb.Schema ({
-  name: {type: String, required: true, trim: true},
-  rating: {type: Array, required: true, trim: true},
-  skills: {type: Array, required: true, trim: true},
-  email: {type: String, required: true, trim: true},
-  phone: {type: String, required: true, trim: true},
-  description: {type: String, required: true, trim: true},
-  image: {type: String, required: true, trim: true},
-projects: {type: String, required: true, trim: true}
+  name: {type: String, required: true}
+  //rating: {type: Array, required: true, trim: true},
+  //skills: {type: Array, required: true, trim: true},
+  //email: {type: String, required: true, trim: true},
+  //phone: {type: String, required: true, trim: true},
+  //description: {type: String, required: true, trim: true},
+  //image: {type: String, required: true, trim: true},
+  //projects: {type: String, required: true, trim: true}
 });
 
 module.exports = mongodb.resource ('user', schema, 'blueprint_user');
--- app/routers/UserRouter.js
+++ app/routers/UserRouter.js
@@ -1,12 +1,7 @@
 module.exports = exports = {
-  //':userID': { action: 'UserController@get'},
-  '/user' : {
-    get  : { view   : 'UserController@get' },
-    post : { action : 'UserController@create'},
-
-    '/:userID': {
-    	get   : { action: 'UserController@get'},
-    	post  : { action: 'UserController@create'}
-    }
-  }
+  '/users' : {
+    	resource: {
+    			controller: 'UserController'
+  		}
+  	}
 };
