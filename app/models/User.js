1ee564e6159360cb49be1600724cd4e4d64f1635
Franz <defranz@indiana.edu>
2017-12-09 04:25:36 -0500

    updated users and seed file

--- app/models/User.js
+++ app/models/User.js
@@ -7,14 +7,14 @@ var mongodb = require ('@onehilltech/blueprint-mongodb')
 
 var schema = new mongodb.Schema ({
   name: {type: String, required: true},
-  rating: {type: Array, trim: true},
-  skills: {type: Array, trim: true},
-  email: {type: String, trim: true},
-  password: {type: String, trim: true},
-  description: {type: String, trim: true},
-  portfolio: {type: String, trim: true},
-  image: {type: String, trim: true},
-  projects: {type: String, trim: true}
+  rating: {type: [Number], required: true, trim: true},
+  skills: {type: [String], required: true, trim: true},
+  email: {type: String, required: true, trim: true},
+  phone: {type: String, required: true, trim: true},
+  description: {type: String, required: true, trim: true},
+  portfolio: {type: String, required: true, trim: true},
+  image: {type: String, required: true, trim: true},
+  projects: {type: [String], required: true, trim: true}
 });
 
 module.exports = mongodb.resource ('user', schema, 'blueprint_user');
--- app/seeds/mongodb/$default.js
+++ app/seeds/mongodb/$default.js
@@ -6,7 +6,37 @@ const dab = require ('@onehilltech/dab')
 module.exports = {
 	users: [
 		{
-			name: 'kyle'
+			name: 'kyle',
+			rating: [1, 2,3],
+			skills: ['Archery','Bowstaffs','Computer Science'],
+			email: 'kyle@gmail.com',
+			phone: '317.000.1111',
+			description: 'Hi, my name is kyle',
+			portfolio: 'someSortOfPort',
+			image: 'someAddress',
+			projects: ['First', 'Second', 'Third']
+		},
+		{
+			name: 'Butch',
+			rating: [4,5,6],
+			skills: ['Interesting','Things','Skills'],
+			email: 'butch@gmail.com',
+			phone: '317.222.3333',
+			description: 'Hi, my name is Butch',
+			portfolio: 'someSortOfFolio',
+			image: 'differentAddress',
+			projects: ['Fourth', 'Fifth', 'Sixth']
+		},
+		{
+			name: 'Justin Bieber',
+			rating: [1,0,1],
+			skills: ['Singing','Hair','Bad decisions'],
+			email: 'biebs@HotMail.com',
+			phone: '317.444.5555',
+			description: 'Hi, my name is Biebs',
+			portfolio: 'PortFolio',
+			image: 'someWhereInLA',
+			projects: ['Seventh', 'Eighth', 'Ninth']
 		}
 	]
 };
